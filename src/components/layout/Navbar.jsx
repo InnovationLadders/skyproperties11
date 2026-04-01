10cf9a4fc7241931c28994e1cda18d05be3406b5
InnovationLadders <mansour@innovationladders.com>
2026-04-01 17:27:39 +0300

    Updated Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -79,6 +79,9 @@ export const Navbar = () => {
             <Link to="/saudi-regulations" className="text-gray-700 hover:text-primary">
               {t('navbar.saudiRegulations')}
             </Link>
+            <Link to="/foreign-services" className="text-gray-700 hover:text-primary">
+              {t('foreignServices.title')}
+            </Link>
 
             {currentUser ? (
               <>
@@ -234,6 +237,20 @@ export const Navbar = () => {
               onClick={() => setMobileMenuOpen(false)}
             >
               {t('publicDirectory.title')}
+            </Link>
+            <Link
+              to="/saudi-regulations"
+              className="block px-3 py-2 text-gray-700 hover:bg-gray-100 rounded-md"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              {t('navbar.saudiRegulations')}
+            </Link>
+            <Link
+              to="/foreign-services"
+              className="block px-3 py-2 text-gray-700 hover:bg-gray-100 rounded-md"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              {t('foreignServices.title')}
             </Link>
 
             {currentUser ? (
